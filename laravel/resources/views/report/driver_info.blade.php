a052defb4e5ae0c05912a7fd0401da7e2b4ddc1d
valen <valentin.kovalenko19955@gmail.com>
2023-08-30 22:04:33 +0200

    c8

--- laravel/resources/views/report/driver_info.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Driver Info</h2>
-@if ($driverInfo)
-    <p>Driver name: {{ $driverInfo['nameRacer'] }}</p>
-    <p>Driver team: {{$driverInfo['team']}}</p>
-    <p>Driver time: {{$driverInfo['lap_time']}}</p>
-@else
-    <p>No information available for this driver.</p>
-@endif
-
