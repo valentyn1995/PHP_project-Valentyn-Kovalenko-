558ade86132238467bdbadacfac314c804d2ddab
valen <valentin.kovalenko19955@gmail.com>
2023-09-07 18:57:58 +0200

    added unittests

---
+++ tests/Unit/Services/BuildReport/CalculateDifferenceTimeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Services\BuildReport\CalculateDifferenceTime;
+use PHPUnit\Framework\TestCase;
+
+class CalculateDifferenceTimeTest extends TestCase
+{
+    public function testCalculatingDifferenceTime()
+    {
+        $calculator = new CalculateDifferenceTime();
+
+        $arrayStart = ['01:01:13.393000', '01:01:15.987000'];
+        $arrayEnd = ['01:02:30.000000', '01:03:00.000000'];
+
+        $expectedDifference = [
+            0 => '00:01:16.607000',
+            1 => '00:01:44.013000',
+        ];
+
+        $calculatedDifference = $calculator->calculatingDifferenceTime($arrayStart, $arrayEnd);
+        $this->assertEquals($expectedDifference, $calculatedDifference);
+    }
+
+    public function testCalculatingDifferenceTimeWithInvalidTimeFormat()
+    {
+        $calculator = new CalculateDifferenceTime();
+
+        $arrayStart = ['01:01:13.393000', 'invalid_time'];
+        $arrayEnd = ['01:02:30.000000', '01:03:00.000000'];
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Error in parsing the format from the report files.');
+
+        $calculator->calculatingDifferenceTime($arrayStart, $arrayEnd);
+    }
+}
---
+++ tests/Unit/Services/BuildReport/ProcessDataRacerServiceTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Services\BuildReport\ExtractDataFromFile;
+use App\Services\BuildReport\ProcessDataRacerService;
+use PHPUnit\Framework\TestCase;
+
+class ProcessDataRacerServiceTest extends TestCase
+{
+    public function testExtractNameRacers()
+    {
+        $extractDataFromFileMock = $this->createMock(ExtractDataFromFile::class);
+        $extractDataFromFileMock->method('extractingDataFromFile')
+            ->willReturn('CSR_Carlos Sainz_RENAULT');
+
+        $processDataRacerService = new ProcessDataRacerService($extractDataFromFileMock);
+
+        $expectedResult = [
+            "CSR" => "Carlos Sainz_RENAULT"
+        ];
+
+        $result = $processDataRacerService->extractNameRacers("path/to");
+
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    public function testExtractNameRacersWithEmptyArg()
+    {
+        $extractDataFromFileMock = $this->createMock(ExtractDataFromFile::class);
+        $extractDataFromFileMock->method('extractingDataFromFile')
+            ->willReturn('');
+
+        $processDataRacerService = new ProcessDataRacerService($extractDataFromFileMock);
+
+        $expectedResult = [];
+
+        $result = $processDataRacerService->extractNameRacers("path/to");
+
+        $this->assertEquals($expectedResult, $result);
+    }
+}
