dc66339a1bf6a6127b896fd7f3524d057df19bb0
valen <valentin.kovalenko19955@gmail.com>
2023-09-13 21:14:57 +0200

    added code fore SwaggerDoc

--- app/Http/Controllers/Api/ReportApiController.php
+++ app/Http/Controllers/Api/ReportApiController.php
@@ -8,14 +8,62 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use App\Services\BuildReport\ReportSortingService;
+use OpenAPI\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="Report API",
+ *     version="1.0",
+ *     description="API for printing report"
+ * )
+ */
 class ReportApiController extends Controller
 {
     public function __construct(private ReportSortingService $reportSortingService)
     {
 
     }
-    public function getReport(Request $request)
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/report",
+     *     summary="get report",
+     *     tags={"Report"},
+     *     @OA\Parameter(
+     *         name="order",
+     *         in="query",
+     *         description="Sort order (asc or desc)",
+     *         required=false,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Parameter(
+     *         name="format",
+     *         in="query",
+     *         description="Response format (json or xml)",
+     *         required=false,
+     *         @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="Driver code",
+     *                 type="object",
+     *                 @OA\Property(property="nameRacer", type="string"),
+     *                 @OA\Property(property="team", type="string"),
+     *                 @OA\Property(property="lap_time", type="string")
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Request error",
+     *     ),
+     * )
+     */
+    public function index(Request $request)
     {
         $pathToFiles = base_path('files_for_report');
         $sortDirection = $request->input('order', 'asc');
