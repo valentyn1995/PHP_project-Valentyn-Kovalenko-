9550c8373f4f7ea33216609f2097288eaae8c714
valen <valentin.kovalenko19955@gmail.com>
2023-10-03 09:32:32 +0200

    setup database

---
+++ database/migrations/2023_09_23_063555_create_reports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->string('drivers_code');
+            $table->string('name');
+            $table->string('team');
+            $table->time('lap_time', 6);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('report');
+    }
+};
