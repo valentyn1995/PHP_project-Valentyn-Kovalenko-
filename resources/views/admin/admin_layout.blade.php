ada1d894df9d4276ef1fe3902704364c4062fe43
valen <valentin.kovalenko19955@gmail.com>
2023-10-14 09:20:49 +0200

    edit synt

--- resources/views/admin/admin_layout.blade.php
+++ resources/views/admin/admin_layout.blade.php
@@ -1,18 +1,16 @@
-<!DOCTYPE html> <html lang="en">
+<!DOCTYPE html> <html lang="en"> <head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-  <title>Report of Monaco Racing</title>
-  <!-- Theme style -->
-  <style>
-    <?php include base_path('vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css');
-    ?>
-  </style>
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<title>Report of Monaco Racing</title>
+<!-- Theme style -->
+<style>
+  <?php include base_path('vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css');
+  ?>
+</style>
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
@@ -56,10 +54,8 @@
       <!-- Main content -->
       <section class="content">
         <div class="container-fluid">
-          <!-- Content Goes Here -->
+          @yield('content')
         </div>
-        @yield('content')
-        <!-- /.card -->
       </section>
       <!-- /.content -->
     </div>
